4a6576a0715acbca0a808eef2a9e4b0f2db9736d
jayendra <65066359+jdragonjds@users.noreply.github.com>
2021-05-03 14:04:30 +0530

    Create d.cpp

---
+++ cf round 14/d.cpp
@@ -0,0 +1,133 @@
+/*     electric love #jdragon
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠤⠖⠚⢉⣩⣭⡭⠛⠓⠲⠦⣄⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⡴⠋⠁⠀⠀⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠳⢦⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⡴⠃⢀⡴⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣆⠀⠀⠀
+⠀⠀⠀⠀⡾⠁⣠⠋⠀⠈⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⠀⠀
+⠀⠀⠀⣸⠁⢰⠃⠀⠀⠀⠈⢣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣇⠀
+⠀⠀⠀⡇⠀⡾⡀⠀⠀⠀⠀⣀⣹⣆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⠀
+⠀⠀⢸⠃⢀⣇⡈⠀⠀⠀⠀⠀⠀⢀⡑⢄⡀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⢸⠀⢻⡟⡻⢶⡆⠀⠀⠀⠀⡼⠟⡳⢿⣦⡑⢄⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⣸⠀⢸⠃⡇⢀⠇⠀⠀⠀⠀⠀⡼⠀⠀⠈⣿⡗⠂⠀⠀⠀⠀⠀⠀⠀⢸⠁
+⠀⠀⡏⠀⣼⠀⢳⠊⠀⠀⠀⠀⠀⠀⠱⣀⣀⠔⣸⠁⠀⠀⠀⠀⠀⠀⠀⢠⡟⠀
+⠀⠀⡇⢀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃⠀
+⠀⢸⠃⠘⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⢀⠀⠀⠀⠀⠀⣾⠀⠀
+⠀⣸⠀⠀⠹⡄⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⠀⠸⠀⠀⠀⠀⠀⡇⠀⠀
+⠀⡏⠀⠀⠀⠙⣆⠀⠀⠀⠀⠀⠀⠀⢀⣠⢶⡇⠀⠀⢰⡀⠀⠀⠀⠀⠀⡇⠀⠀
+⢰⠇⡄⠀⠀⠀⡿⢣⣀⣀⣀⡤⠴⡞⠉⠀⢸⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⣧⠀⠀
+⣸⠀⡇⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⢹⠀⠀⢸⠀⠀⢀⣿⠇⠀⠀⠀⠁⠀⢸⠀⠀
+⣿⠀⡇⠀⠀⠀⠀⠀⢀⡤⠤⠶⠶⠾⠤⠄⢸⠀⡀⠸⣿⣀⠀⠀⠀⠀⠀⠈⣇⠀
+⡇⠀⡇⠀⠀⡀⠀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠸⡌⣵⡀⢳⡇⠀⠀⠀⠀⠀⠀⢹⡀
+⡇⠀⠇⠀⠀⡇⡸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠮⢧⣀⣻⢂⠀⠀⠀⠀⠀⠀⢧
+⣇⠀⢠⠀⠀⢳⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡎⣆⠀⠀⠀⠀⠀⠘
+⢻⠀⠈⠰⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⠘⢮⣧⡀⠀⠀⠀⠀
+⠸⡆⠀⠀⠇⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠆⠀⠀⠀⠀⠀⠀⠀⠙⠳⣄⡀⢢⡀
+ oregairu */ 
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long
+#define fl i,a,b for(int i=a;i<b;i++)
+#define co cout<<
+#define ci cin>>
+#define rep(i,x,n) for(long long i = x; i < n; i++) 
+#define rev(i,n,x) for(long long i = n; i >= x; --i)
+#define ff            first
+#define ss            second
+#define vecll vector<long long>
+#define pii pair<long long ,long long >
+#define pip pair<int,pair<int,int>>
+#define ppp pair<pair<int,int>,pair<int,int>>
+#define inarr(arr,x,n)  for(ll i=x ;i<n;i++) cin >> arr[i];
+#define carr(v,x,n)  for(ll i=x ;i<n;i++) cout << v[i]-v[i-1]<<" ";
+#define mpp make_pair
+#define pb               push_back
+#define ppb               pop_back
+#define all(x)          (x).begin(),(x).end()
+#define sz(x)           (x).size()
+#define henshin           cout<<endl;
+#define DEBUG(x) cout << (x) << '\n';
+#define int long long 
+const ll oo = 1e18;
+inline void yn(int x) { if(!x)cout << "NO\n";  else if(x)cout << "YES\n";return; }
+
+//const ll N=1e7+1,M=1e5+7,mod=1e9+7;
+  const int MAXA = 1e7+5; 
+ int mind[MAXA+5]={0};
+
+  
+ int N,L,R;
+int C[200001];
+int lcnt[200001],rcnt[200001];
+
+
+
+void solve(){
+ cin>>N>>L>>R;
+  for (int i=1;i<=N;i++){
+    lcnt[i]=0;
+    rcnt[i]=0;
+  }
+  for (int i=1;i<=N;i++){
+    cin>>C[i];
+    if (i<=L)
+      lcnt[C[i]]++;
+    else
+      rcnt[C[i]]++;
+  } 
+  for (int i=1;i<=N;i++){
+    int mn=min(lcnt[i],rcnt[i]);
+    lcnt[i]-=mn;
+    rcnt[i]-=mn;
+    L-=mn;
+    R-=mn;
+  }
+  if (L<R){
+    swap(lcnt,rcnt);
+    swap(L,R);
+  }
+ int ans=0;
+  for (int i=1;i<=N;i++){
+    int extra=L-R;  
+    int canDo=lcnt[i]/2;
+    int Do=min(canDo*2,extra);
+     
+    ans+=Do/2;// ls of same color cost+1 otherwise we change color and side +2
+    L-=Do;
+  }
+   
+  ans+=(L-R)/2+(L+R)/2;
+  cout<<ans<<endl;
+ 
+
+ } 
+
+
+
+
+
+ 
+signed main() {
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    cout << fixed << setprecision(15);
+
+/*  
+for (int i = 2; i * i < N; i++) {
+    if (mind[i] == 0) {
+      mind[i] = i;
+      for (int j = i * i; j < N; j += i) {
+        if (mind[j] == 0) {
+          mind[j] = i;
+        }
+      }
+    }
+  }
+*/
+    
+    int tc=1;   
+    ci tc;
+    while(tc--) { 
+    solve();
+    }
+    return 0;
+}
