30465087642297c8656ea2b745c83bfd2a15eded
jayendra <65066359+jdragonjds@users.noreply.github.com>
2021-05-05 15:39:56 +0530

    Create 647 (Div. 1)  (B) Johnny and Grandmaster.cpp

---
+++ binary,constructive ,1700-1900/647 (Div. 1)  (B) Johnny and Grandmaster.cpp
@@ -0,0 +1,227 @@
+/*     electric love #jdragon
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠤⠖⠚⢉⣩⣭⡭⠛⠓⠲⠦⣄⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⡴⠋⠁⠀⠀⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠳⢦⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⡴⠃⢀⡴⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣆⠀⠀⠀
+⠀⠀⠀⠀⡾⠁⣠⠋⠀⠈⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⠀⠀
+⠀⠀⠀⣸⠁⢰⠃⠀⠀⠀⠈⢣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣇⠀
+⠀⠀⠀⡇⠀⡾⡀⠀⠀⠀⠀⣀⣹⣆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⠀
+⠀⠀⢸⠃⢀⣇⡈⠀⠀⠀⠀⠀⠀⢀⡑⢄⡀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⢸⠀⢻⡟⡻⢶⡆⠀⠀⠀⠀⡼⠟⡳⢿⣦⡑⢄⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⣸⠀⢸⠃⡇⢀⠇⠀⠀⠀⠀⠀⡼⠀⠀⠈⣿⡗⠂⠀⠀⠀⠀⠀⠀⠀⢸⠁
+⠀⠀⡏⠀⣼⠀⢳⠊⠀⠀⠀⠀⠀⠀⠱⣀⣀⠔⣸⠁⠀⠀⠀⠀⠀⠀⠀⢠⡟⠀
+⠀⠀⡇⢀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃⠀
+⠀⢸⠃⠘⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⢀⠀⠀⠀⠀⠀⣾⠀⠀
+⠀⣸⠀⠀⠹⡄⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⠀⠸⠀⠀⠀⠀⠀⡇⠀⠀
+⠀⡏⠀⠀⠀⠙⣆⠀⠀⠀⠀⠀⠀⠀⢀⣠⢶⡇⠀⠀⢰⡀⠀⠀⠀⠀⠀⡇⠀⠀
+⢰⠇⡄⠀⠀⠀⡿⢣⣀⣀⣀⡤⠴⡞⠉⠀⢸⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⣧⠀⠀
+⣸⠀⡇⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⢹⠀⠀⢸⠀⠀⢀⣿⠇⠀⠀⠀⠁⠀⢸⠀⠀
+⣿⠀⡇⠀⠀⠀⠀⠀⢀⡤⠤⠶⠶⠾⠤⠄⢸⠀⡀⠸⣿⣀⠀⠀⠀⠀⠀⠈⣇⠀
+⡇⠀⡇⠀⠀⡀⠀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠸⡌⣵⡀⢳⡇⠀⠀⠀⠀⠀⠀⢹⡀
+⡇⠀⠇⠀⠀⡇⡸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠮⢧⣀⣻⢂⠀⠀⠀⠀⠀⠀⢧
+⣇⠀⢠⠀⠀⢳⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡎⣆⠀⠀⠀⠀⠀⠘
+⢻⠀⠈⠰⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⠘⢮⣧⡀⠀⠀⠀⠀
+⠸⡆⠀⠀⠇⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠆⠀⠀⠀⠀⠀⠀⠀⠙⠳⣄⡀⢢⡀
+ oregairu */ 
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long
+#define fl i,a,b for(int i=a;i<b;i++)
+#define co cout<<
+#define ci cin>>
+#define rep(i,x,n) for(long long i = x; i < n; i++) 
+#define rev(i,n,x) for(long long i = n; i >= x; --i)
+#define ff            first
+#define ss            second
+#define vecll vector<long long>
+#define pii pair<long long ,long long >
+#define pip pair<int,pair<int,int>>
+#define ppp pair<pair<int,int>,pair<int,int>>
+#define inarr(arr,x,n)  for(ll i=x ;i<n;i++) cin >> arr[i];
+#define carr(v,x,n)  for(ll i=x ;i<n;i++) cout << v[i]-v[i-1]<<" ";
+#define mpp make_pair
+#define pb               push_back
+#define ppb               pop_back
+#define all(x)          (x).begin(),(x).end()
+#define rall(x)          (x).rbegin(),(x).rend()
+#define sz(x)           (x).size()
+#define henshin           cout<<endl;
+#define DEBUG(x) cout << (x) << '\n';
+#define int long long 
+const ll oo = 1e18;
+inline void yn(int x) { if(!x)cout << "NO\n";  else if(x)cout << "YES\n";return; }
+
+ const ll N=1e12+2,M=1e6 ,mod=1e9+7;
+  const int MAXA = 1e7+5; 
+ int mind[MAXA+5]={0};
+
+ 
+ 
+ll fexp(ll a, ll b ) {
+    a %= mod;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+void solve(){ll n,p;
+    ci n>>p; 
+ ll sum=0;
+ 
+ if(p==1){ 
+    co n%2<<endl;
+    rep(i,0,n)ci sum;
+    return;  
+ }
+
+
+
+
+vector<int> K(n);
+ 
+for(auto &ki:K){  ci ki;  }
+    
+ 
+sort(rall(K));
+vector<pii> p1,p2;
+      int i=0,j=0,x;
+       rep(i,0,n)
+       {
+          if(p1.size()==0)
+          {
+            p1.pb({K[i],1});
+          }
+          else if(p2.size()==0)
+          {
+            p2.pb({K[i],1});
+          }
+          else if(p1[p1.size()-1]==p2[p2.size()-1])
+          {
+            x=p1.size();
+            if(p1[x-1].ff==K[i])
+              {
+                  p1[x-1].ss++;
+                  while(p1[x-1].ss==p)
+                  {
+                    auto h1=p1[x-1];
+                    p1.pop_back();
+                    x=p1.size();
+                    if(x==0) p1.pb({h1.ff+1,1});
+                    else
+                    {
+                      if(p1[x-1].ff==h1.ff+1)
+                      {
+                        p1[x-1].ss++;
+                        continue;
+                      }
+                      else
+                      {
+                        p1.pb({h1.ff+1,1});
+                        x++;
+                        continue;
+                      }
+                    }
+                  }
+                  
+              }
+              else
+              {
+                p1.pb({K[i],1});
+              }
+          }
+          else
+          {
+            if(p2.size()==0)
+            {
+              p2.pb({K[i],1});
+            }
+            else
+            {
+             x=p2.size();
+              if(p2[x-1].ff==K[i])
+              {
+                  p2[x-1].ss++;
+                  while(p2[x-1].ss==p)
+                  {
+                    auto h1=p2[x-1];
+                    p2.pop_back();
+                    x=p2.size();
+                    if(x==0) p2.pb({h1.ff+1,1});
+                    else
+                    {
+                      if(p2[x-1].ff==h1.ff+1)
+                      {
+                        p2[x-1].ss++;
+                        continue;
+                      }
+                      else
+                      {
+                        p2.pb({h1.ff+1,1});
+                        x++;
+                        continue;
+                      }
+                    }
+                  }
+                  
+              }
+              else
+              {
+                p2.pb({K[i],1});
+              }
+            }
+          }
+       }
+  
+ll t1=0;
+       rep(i,0,p1.size())
+       {
+          //cout << p1[i].fi << " " << p1[i].se << endl ;
+          t1+=(p1[i].ss*fexp(p,p1[i].ff))%mod;
+          t1%=mod;
+       }
+       ll t2=0;
+       rep(i,0,p2.size())
+       {
+          t2+=(p2[i].ss*fexp(p,p2[i].ff))%mod;
+          t2%=mod;
+       }
+
+
+
+DEBUG((t1-t2+mod)%mod)} 
+
+
+
+
+
+ 
+signed main() {
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    cout << fixed << setprecision(15);
+
+/*  
+for (int i = 2; i * i < N; i++) {
+    if (mind[i] == 0) {
+      mind[i] = i;
+      for (int j = i * i; j < N; j += i) {
+        if (mind[j] == 0) {
+          mind[j] = i;
+        }
+      }
+    }
+  }
+*/
+    
+    int tc=1;   
+     ci tc;
+    while(tc--) { 
+    solve();
+    }
+    return 0;
+}
