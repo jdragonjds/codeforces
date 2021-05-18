f71e17186c6a30c2dc6b9a98a990e276e183344f
jayendra <65066359+jdragonjds@users.noreply.github.com>
2021-05-18 18:27:46 +0530

    Create d.cpp

---
+++ div 2 720/d.cpp
@@ -0,0 +1,209 @@
+/*     electric love #jdragon
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠤⠖⠚⢉⣩⣭⡭⠛⠓⠲⠦⣄⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⡴⠋⠁⠀⠀⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠳⢦⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⡴⠃⢀⡴⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣆⠀⠀⠀
+⠀⠀⠀⠀⡾⠁⣠⠋⠀⠈⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⠀⠀
+⠀⠀⠀⣸⠁⢰⠃⠀⠀⠀⠈⢣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣇⠀
+⠀⠀⠀⡇⠀⡾⡀⠀⠀⠀⠀⣀⣹⣆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⠀
+⠀⠀⢸⠃⢀⣇⡈⠀⠀⠀⠀⠀⠀⢀⡑⢄⡀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⢸⠀⢻⡟⡻⢶⡆⠀⠀⠀⠀⡼⠟⡳⢿⣦⡑⢄⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⣸⠀⢸⠃⡇⢀⠇⠀⠀⠀⠀⠀⡼⠀⠀⠈⣿⡗⠂⠀⠀⠀⠀⠀⠀⠀⢸⠁
+⠀⠀⡏⠀⣼⠀⢳⠊⠀⠀⠀⠀⠀⠀⠱⣀⣀⠔⣸⠁⠀⠀⠀⠀⠀⠀⠀⢠⡟⠀
+⠀⠀⡇⢀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃⠀
+⠀⢸⠃⠘⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⢀⠀⠀⠀⠀⠀⣾⠀⠀
+⠀⣸⠀⠀⠹⡄⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⠀⠸⠀⠀⠀⠀⠀⡇⠀⠀
+⠀⡏⠀⠀⠀⠙⣆⠀⠀⠀⠀⠀⠀⠀⢀⣠⢶⡇⠀⠀⢰⡀⠀⠀⠀⠀⠀⡇⠀⠀
+⢰⠇⡄⠀⠀⠀⡿⢣⣀⣀⣀⡤⠴⡞⠉⠀⢸⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⣧⠀⠀
+⣸⠀⡇⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⢹⠀⠀⢸⠀⠀⢀⣿⠇⠀⠀⠀⠁⠀⢸⠀⠀
+⣿⠀⡇⠀⠀⠀⠀⠀⢀⡤⠤⠶⠶⠾⠤⠄⢸⠀⡀⠸⣿⣀⠀⠀⠀⠀⠀⠈⣇⠀
+⡇⠀⡇⠀⠀⡀⠀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠸⡌⣵⡀⢳⡇⠀⠀⠀⠀⠀⠀⢹⡀
+⡇⠀⠇⠀⠀⡇⡸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠮⢧⣀⣻⢂⠀⠀⠀⠀⠀⠀⢧
+⣇⠀⢠⠀⠀⢳⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡎⣆⠀⠀⠀⠀⠀⠘
+⢻⠀⠈⠰⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⠘⢮⣧⡀⠀⠀⠀⠀
+⠸⡆⠀⠀⠇⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠆⠀⠀⠀⠀⠀⠀⠀⠙⠳⣄⡀⢢⡀
+ oregairu */ 
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long
+#define fl i,a,b for(int i=a;i<b;i++)
+#define co cout<<
+#define ci cin>>
+#define rep(i,x,n) for(long long i = x; i < n; i++) 
+#define rev(i,n,x) for(long long i = n; i >= x; --i)
+#define ff            first
+#define ss            second
+#define vecll vector<long long>
+#define pii pair<long long ,long long >
+#define pip pair<int,pair<int,int>>
+#define ppp pair<pair<int,int>,pair<int,int>>
+#define inarr(arr,x,n)  for(ll i=x ;i<n;i++) cin >> arr[i];
+#define carr(v,x,n)  for(ll i=x ;i<n;i++) cout << v[i]-v[i-1]<<" ";
+#define mpp make_pair
+#define pb               push_back
+#define ppb               pop_back
+#define all(x)          (x).begin(),(x).end()
+#define rall(x)          (x).rbegin(),(x).rend()
+#define sz(x)           (x).size()
+#define henshin           cout<<endl;
+#define DEBUG(x) cout << (x) << '\n';
+#define int long long 
+const ll oo = 1e18;
+inline void yn(int x) { if(!x)cout << "NO\n";  else if(x)cout << "YES\n";return; }
+
+ const ll N=1e12+2,M=1e6 ,mod=1e9+7;
+  const int MAXA = 2077; 
+ int mind[MAXA+5]={0};
+
+ vector<int> cnt(26); 
+  /*
+int ques(int t, int i, int j, int x) {
+    cout << "? " << t << ' ' << i + 1 << ' ' << j + 1 << ' ' << x << endl;
+    int val; cin >> val;
+    if (val == -1) exit(0);
+    return val;
+}    */
+
+
+struct edge {
+    int v, u;
+};
+vector<pair<edge, edge>> operations;
+
+int dp[N], answer = 0;
+
+bool isDeleted[N];
+
+vector<pair<int, int>> g[N];
+
+void dfs(int v, int p = -1) {
+    int sz = (int)g[v].size() - (p != -1);
+    for (auto to : g[v]) {
+        if (to.first == p) continue;
+        dfs(to.first, v);
+        if (dp[to.first]) {
+            --sz; ++answer;
+            isDeleted[to.second] = true;
+        }
+    }
+    if (sz >= 2) {
+        dp[v] = true;
+        for (auto to : g[v]) {
+            if (to.first == p) continue;
+            if (sz <= 2) break;
+            if (!dp[to.first]) {
+                --sz; ++answer;
+                isDeleted[to.second] = true;
+            }
+        }
+    }
+}
+
+vector<pair<int, int>> bamboos;
+
+bool used[N];
+
+vector<int> leaves;
+
+void dfs2(int v, int root) {
+    used[v] = true;
+    int numberOfChildren = 0;
+    for (auto to : g[v]) {
+        if (used[to.first] || isDeleted[to.second]) continue;
+        ++numberOfChildren; dfs2(to.first, root);
+    }
+    if (v == root && numberOfChildren == 1) {
+        leaves.push_back(v);
+    } else if (!numberOfChildren) {
+        leaves.push_back(v);
+    }
+}
+
+void clear(int n) {
+    answer = 0;
+    for (int i = 0; i < n; ++i) {
+        dp[i] = 0;
+        g[i].clear();
+        used[i] = isDeleted[i] = false;
+    }
+    bamboos.clear();
+    operations.clear();
+} 
+
+signed main() {
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    cout << fixed << setprecision(15);
+ /*
+for (int i = 2; i * i < MAXA; i++) {
+    if (mind[i] == 0) {
+      mind[i] = i;
+      for (int j = i * i; j < MAXA; j += i) {
+        if (mind[j] == 0) {
+          mind[j] = i;
+        }
+      }
+    }
+  }
+ */
+    
+    int tc=1;   
+      ci tc;
+
+    while(tc--) { 
+
+int n; cin >> n;
+
+        for (int i = 0; i < n - 1; ++i) {
+            int a, b; cin >> a >> b;
+            --a; --b;
+            g[a].push_back({b, i});
+            g[b].push_back({a, i});
+        }
+
+        dfs(0);
+
+//bamboo forest into bamboo
+
+for (int i = 0; i < n; ++i) {
+            if (!used[i]) {
+                dfs2(i, i);
+                assert((int)leaves.size() <= 2);
+                if ((int)leaves.size() == 2) {
+                    bamboos.push_back({leaves[0], leaves[1]});
+                }
+                if ((int)leaves.size() == 1) {
+                    bamboos.push_back({leaves.back(), leaves.back()});
+                }
+                leaves.clear();
+            }
+        }
+
+
+ vector<edge> deletedEdges, addedEdges;
+        for (int v = 0; v < n; ++v) {
+            for (auto to : g[v]) {
+                if (isDeleted[to.second]) {
+                    if (v < to.first) {
+                        deletedEdges.push_back({v, to.first});
+                    }
+                }
+            }
+        }
+        for (int i = 1; i < (int)bamboos.size(); ++i) {
+            addedEdges.push_back({bamboos[i - 1].second, bamboos[i].first});
+        }
+
+
+
+cout << answer << endl;
+        for (pair<edge, edge> to : operations) {
+            cout << to.first.v + 1 << ' ' << to.first.u + 1 << ' ';
+            cout << to.second.v + 1 << ' ' << to.second.u + 1 << endl;
+        }
+
+        clear(n);
+
+ }
+    return 0;
+}
