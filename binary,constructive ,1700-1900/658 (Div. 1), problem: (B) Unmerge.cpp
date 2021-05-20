80c1927d4f36eeeb40be795daa3508db69d9bca8
jayendra <65066359+jdragonjds@users.noreply.github.com>
2021-05-20 14:38:21 +0530

    Create 658 (Div. 1), problem: (B) Unmerge.cpp

---
+++ binary,constructive ,1700-1900/658 (Div. 1), problem: (B) Unmerge.cpp
@@ -0,0 +1,133 @@
+/*     electric love #jai shree RAM
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠤⠖⠚⢉⣩⣭⡭⠛⠓⠲⠦⣄⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⡴⠋⠁⠀⠀⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠳⢦⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⡴⠃⢀⡴⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣆⠀⠀⠀
+⠀⠀⠀⠀⡾⠁⣠⠋⠀⠈⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⠀⠀
+⠀⠀⠀⣸⠁⢰⠃⠀⠀⠀⠈⢣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣇⠀
+⠀⠀⠀⡇⠀⡾⡀⠀⠀⠀⠀⣀⣹⣆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⠀
+⠀⠀⢸⠃⢀⣇⡈⠀⠀⠀⠀⠀⠀⢀⡑⢄⡀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⢸⠀⢻⡟⡻⢶⡆⠀⠀⠀⠀⡼⠟⡳⢿⣦⡑⢄⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⣸⠀⢸⠃⡇⢀⠇⠀⠀⠀⠀⠀⡼⠀⠀⠈⣿⡗⠂⠀⠀⠀⠀⠀⠀⠀⢸⠁
+⠀⠀⡏⠀⣼⠀⢳⠊⠀⠀⠀⠀⠀⠀⠱⣀⣀⠔⣸⠁⠀⠀⠀⠀⠀⠀⠀⢠⡟⠀
+⠀⠀⡇⢀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃⠀
+⠀⢸⠃⠘⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⢀⠀⠀⠀⠀⠀⣾⠀⠀
+⠀⣸⠀⠀⠹⡄⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⠀⠸⠀⠀⠀⠀⠀⡇⠀⠀
+⠀⡏⠀⠀⠀⠙⣆⠀⠀⠀⠀⠀⠀⠀⢀⣠⢶⡇⠀⠀⢰⡀⠀⠀⠀⠀⠀⡇⠀⠀
+⢰⠇⡄⠀⠀⠀⡿⢣⣀⣀⣀⡤⠴⡞⠉⠀⢸⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⣧⠀⠀
+⣸⠀⡇⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⢹⠀⠀⢸⠀⠀⢀⣿⠇⠀⠀⠀⠁⠀⢸⠀⠀
+⣿⠀⡇⠀⠀⠀⠀⠀⢀⡤⠤⠶⠶⠾⠤⠄⢸⠀⡀⠸⣿⣀⠀⠀⠀⠀⠀⠈⣇⠀
+⡇⠀⡇⠀⠀⡀⠀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠸⡌⣵⡀⢳⡇⠀⠀⠀⠀⠀⠀⢹⡀
+⡇⠀⠇⠀⠀⡇⡸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠮⢧⣀⣻⢂⠀⠀⠀⠀⠀⠀⢧
+⣇⠀⢠⠀⠀⢳⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡎⣆⠀⠀⠀⠀⠀⠘
+⢻⠀⠈⠰⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⠘⢮⣧⡀⠀⠀⠀⠀
+⠸⡆⠀⠀⠇⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠆⠀⠀⠀⠀⠀⠀⠀⠙⠳⣄⡀⢢⡀
+ oregairu */ 
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long
+#define fl i,a,b for(int i=a;i<b;i++)
+#define co cout<<
+#define ci cin>>
+#define rep(i,x,n) for(long long i = x; i < n; i++) 
+#define rev(i,n,x) for(long long i = n; i >= x; --i)
+#define ff            first
+#define ss            second
+#define vecll vector<long long>
+#define pii pair<long long ,long long >
+#define pip pair<int,pair<int,int>>
+#define ppp pair<pair<int,int>,pair<int,int>>
+#define inarr(arr,x,n)  for(ll i=x ;i<n;i++) cin >> arr[i];
+#define inar(ar)  for (auto &i : ar){cin >> i;}
+#define carr(v,x,n)  for(ll i=x ;i<n;i++) cout << v[i]-v[i-1]<<" ";
+#define mpp make_pair
+#define pb               push_back
+#define ppb               pop_back
+#define all(x)          (x).begin(),(x).end()
+#define rall(x)          (x).rbegin(),(x).rend()
+#define sz(x)           (x).size()
+#define henshin           cout<<endl;
+#define DEBUG(x) cout << (x) << '\n';
+#define int long long 
+const ll oo = 1e18;
+inline void yn(int x) { if(!x)cout << "NO\n";  else if(x)cout << "YES\n";return; }
+
+  const int N = 2e6+7 ,M=1e6 ,mod=1e9+7;
+  const int MAXA = 0; 
+ int mind[MAXA+5]={0};
+ 
+  /*
+int ques(int t, int i, int j, int x) {
+    cout << "? " << t << ' ' << i + 1 << ' ' << j + 1 << ' ' << x << endl;
+    int val; cin >> val;
+    if (val == -1) exit(0);
+    return val;
+}    */
+ 
+ 
+
+signed main() {
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    cout << fixed << setprecision(15);
+ /*
+for (int i = 2; i * i < MAXA; i++) {
+    if (mind[i] == 0) {
+      mind[i] = i;
+      for (int j = i * i; j < MAXA; j += i) {
+        if (mind[j] == 0) {
+          mind[j] = i;
+        }
+      }
+    }
+  }
+ */
+   
+
+ int tc=1;  ci tc; 
+
+ 
+while(tc--) { 
+    ll n,  x=0,ans=0; 
+    cin >> n ; vecll p(2*n); 
+for (auto &i : p){cin >> i;}
+
+vecll v; ll mak = p[0] , cnt = 1;
+    
+    for (int i = 1; i <2*n; i++)
+    {
+        if (mak > p[i])
+            cnt++;
+        else
+        {
+            mak = p[i];
+            v.push_back(cnt);
+            cnt = 1;
+        }
+    }
+   if (cnt) v.push_back(cnt);
+p.clear();
+    ll vn = (v.size()),sum = 0;
+
+    for (int i = 0; i < vn; i++)
+        sum += v[i];
+    if (sum % 2 || sum / 2 != n)
+       { cout<< "NO\n";continue;}
+    sort(rall(v));
+    // verify if subseq(v) sum is == n 
+ 
+    vecll dp(n*2+5,0);
+    dp[v[0]]=1;  
+    if(!dp[n])
+    rep(i,1,vn){ 
+        for(int j=n;j>0;j--)
+        if(dp[j]){dp[v[i]+j]=1;}
+
+        dp[v[i]]=1;
+        if(dp[n])break; 
+    } 
+     (dp[n])?yn(1):yn(0);
+
+ }
+    return 0;
+} 
