21db40690248c5ca6bb15e48b9d4f834debb5988
jayendra <65066359+jdragonjds@users.noreply.github.com>
2021-06-11 19:17:20 +0530

    Create 1096D - Easy Problem.cpp

---
+++ binary,constructive ,1700-1900/1096D - Easy Problem.cpp
@@ -0,0 +1,145 @@
+/*     electric love #jai shree RAM
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠤⠖⠚⢉⣩⣭⡭⠛⠓⠲⠦⣄⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⡴⠋⠁⠀⠀⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠳⢦⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⡴⠃⢀⡴⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣆⠀⠀⠀
+⠀⠀⠀⠀⡾⠁⣠⠋⠀⠈⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⠀⠀
+⠀⠀⠀⣸⠁⢰⠃⠀⠀⠀⠈⢣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣇⠀
+⠀⠀⠀⡇⠀⡾⡀⠀⠀⠀⠀⣀⣹⣆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⠀
+⠀⠀⢸⠃⢀⣇⡈⠀⠀⠀⠀⠀⠀⢀⡑⢄⡀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⢸⠀⢻⡟⡻⢶⡆⠀⠀⠀⠀⡼⠟⡳⢿⣦⡑⢄⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⣸⠀⢸⠃⡇⢀⠇⠀⠀⠀⠀⠀⡼⠀⠀⠈⣿⡗⠂⠀⠀⠀⠀⠀⠀⠀⢸⠁
+⠀⠀⡏⠀⣼⠀⢳⠊⠀⠀⠀⠀⠀⠀⠱⣀⣀⠔⣸⠁⠀⠀⠀⠀⠀⠀⠀⢠⡟⠀
+⠀⠀⡇⢀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃⠀
+⠀⢸⠃⠘⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⢀⠀⠀⠀⠀⠀⣾⠀⠀
+⠀⣸⠀⠀⠹⡄⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⠀⠸⠀⠀⠀⠀⠀⡇⠀⠀
+⠀⡏⠀⠀⠀⠙⣆⠀⠀⠀⠀⠀⠀⠀⢀⣠⢶⡇⠀⠀⢰⡀⠀⠀⠀⠀⠀⡇⠀⠀
+⢰⠇⡄⠀⠀⠀⡿⢣⣀⣀⣀⡤⠴⡞⠉⠀⢸⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⣧⠀⠀
+⣸⠀⡇⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⢹⠀⠀⢸⠀⠀⢀⣿⠇⠀⠀⠀⠁⠀⢸⠀⠀
+⣿⠀⡇⠀⠀⠀⠀⠀⢀⡤⠤⠶⠶⠾⠤⠄⢸⠀⡀⠸⣿⣀⠀⠀⠀⠀⠀⠈⣇⠀
+⡇⠀⡇⠀⠀⡀⠀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠸⡌⣵⡀⢳⡇⠀⠀⠀⠀⠀⠀⢹⡀
+⡇⠀⠇⠀⠀⡇⡸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠮⢧⣀⣻⢂⠀⠀⠀⠀⠀⠀⢧
+⣇⠀⢠⠀⠀⢳⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡎⣆⠀⠀⠀⠀⠀⠘
+⢻⠀⠈⠰⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⠘⢮⣧⡀⠀⠀⠀⠀
+⠸⡆⠀⠀⠇⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠆⠀⠀⠀⠀⠀⠀⠀⠙⠳⣄⡀⢢⡀
+ oregairu */ 
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long 
+#define co cout<<
+#define ci cin>>
+#define rep(i,x,n) for(long long i = x; i < n; i++) 
+#define rev(i,n,x) for(long long i = n-1; i >= x; --i)
+#define ff            first
+#define ss            second 
+#define pii pair<long long ,long long >
+#define pip pair<int,pair<int,int>>
+#define ppp pair<pair<int,int>,pair<int,int>>
+#define inarr(arr,x,n)  for(ll i=x ;i<n;i++) cin >> arr[i];
+#define inar(ar)  for (auto &i : ar){cin >> i;}
+#define carr(v,x,n)  for(ll i=x ;i<n;i++) cout << v[i]-v[i-1]<<" ";
+#define mpp make_pair
+#define pb                push_back 
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define all(x)          (x).begin(),(x).end()
+#define rall(x)          (x).rbegin(),(x).rend()
+#define sz(x)           (x).size()
+#define henshin           cout<<endl;
+#define DEBUG(x) cout << (x) << '\n';
+#define LSB(i) ((i) & -(i))
+#define int long long 
+const ll oo = 1e18;
+inline void yn(int x) { if(x)co "YES\n"; else co "NO\n"; return; }
+
+  const int N = 1e5 + 7 ,M=1e6 ,mod=1e9+7,mod1=998244353;
+  const int MAXA = 0; 
+  int mind[MAXA+5]={0};
+ /*  
+int ques(int t, int i, int j ) {
+//    cout << "? " << t << ' ' << i + 1 << ' ' << j + 1 << ' ' << x << endl;
+ 
+ int val; cin >> val;
+ if (val == -1) exit(0);
+ return val;
+}  
+int binpow (int a, int n) {
+    int res = 1;
+    while (n) {
+        if (n & 1)
+            res = 1ll * res * a % base;
+        a = 1ll * a * a % base;
+        n >>= 1;
+    }
+    return res;
+}
+ 
+*/
+/*  
+ll gcd(ll u, ll v)
+{
+    int shift;
+    if (u == 0) return v;
+    if (v == 0) return u;
+    shift = __builtin_ctz(u | v);
+    u >>= __builtin_ctz(u);
+    do {
+        v >>= __builtin_ctz(v);
+        if (u > v) {
+            ll t = v;
+            v = u;
+            u = t;
+        }  
+        v = v - u;
+    } while (v != 0);
+    return u << shift;
+}
+*/ 
+
+ 
+vector<int> a;
+ int ans;
+ll dp[N][5];
+
+void solve(){  
+int n;
+    cin >> n;
+    a = vector<int>(n);
+    vector<int> c(4,0);
+    string s; ci s; 
+rep(i,0,n) ci a[i];
+     int j=0; 
+   string ss="hard";
+rep(i,0,n+2) rep(j,0,5) dp[i][j] = oo;
+    dp[0][0] = 0;
+    rep(i,0,n) rep(j,0,4){
+        dp[i + 1][j + (s[i] == ss[j])] = min(dp[i + 1][j + (s[i] == ss[j])], dp[i][j]);
+        dp[i + 1][j] = min(dp[i + 1][j], dp[i][j] + a[i]);
+    }
+
+
+ DEBUG(*min_element(dp[n], dp[n] + 4))}//solve
+
+signed main() {
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    cout << fixed << setprecision(15);
+ /*
+for (int i = 2; i * i < MAXA; i++) {
+    if (mind[i] == 0) {
+      mind[i] = i;
+      for (int j = i * i; j < MAXA; j += i) {
+        if (mind[j] == 0) {
+          mind[j] = i;
+        }
+      }
+    }
+  }
+ */
+   
+int tc=1;    // ci tc; 
+ 
+while(tc--) {
+solve();
+}return 0;} 
