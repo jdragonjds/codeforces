d613d7314e369d7c35fcd6736321cc85bb312948
jayendra <65066359+jdragonjds@users.noreply.github.com>
2021-06-14 18:01:03 +0530

    Create Educational Codeforces Round 61 (Rated for Div. 2), problem: (F) Clear the String.cpp

---
+++ epic 2000+/Educational Codeforces Round 61 (Rated for Div. 2), problem: (F) Clear the String.cpp
@@ -0,0 +1,127 @@
+/*     electric love #jai shree RAM
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠤⠖⠚⢉⣩⣭⡭⠛⠓⠲⠦⣄⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⡴⠋⠁⠀⠀⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠳⢦⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⡴⠃⢀⡴⢳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣆⠀⠀⠀
+⠀⠀⠀⠀⡾⠁⣠⠋⠀⠈⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⠀⠀
+⠀⠀⠀⣸⠁⢰⠃⠀⠀⠀⠈⢣⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣇⠀
+⠀⠀⠀⡇⠀⡾⡀⠀⠀⠀⠀⣀⣹⣆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⠀
+⠀⠀⢸⠃⢀⣇⡈⠀⠀⠀⠀⠀⠀⢀⡑⢄⡀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⢸⠀⢻⡟⡻⢶⡆⠀⠀⠀⠀⡼⠟⡳⢿⣦⡑⢄⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⣸⠀⢸⠃⡇⢀⠇⠀⠀⠀⠀⠀⡼⠀⠀⠈⣿⡗⠂⠀⠀⠀⠀⠀⠀⠀⢸⠁
+⠀⠀⡏⠀⣼⠀⢳⠊⠀⠀⠀⠀⠀⠀⠱⣀⣀⠔⣸⠁⠀⠀⠀⠀⠀⠀⠀⢠⡟⠀
+⠀⠀⡇⢀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃⠀
+⠀⢸⠃⠘⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⢀⠀⠀⠀⠀⠀⣾⠀⠀
+⠀⣸⠀⠀⠹⡄⠀⠀⠈⠁⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⠀⠸⠀⠀⠀⠀⠀⡇⠀⠀
+⠀⡏⠀⠀⠀⠙⣆⠀⠀⠀⠀⠀⠀⠀⢀⣠⢶⡇⠀⠀⢰⡀⠀⠀⠀⠀⠀⡇⠀⠀
+⢰⠇⡄⠀⠀⠀⡿⢣⣀⣀⣀⡤⠴⡞⠉⠀⢸⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⣧⠀⠀
+⣸⠀⡇⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⢹⠀⠀⢸⠀⠀⢀⣿⠇⠀⠀⠀⠁⠀⢸⠀⠀
+⣿⠀⡇⠀⠀⠀⠀⠀⢀⡤⠤⠶⠶⠾⠤⠄⢸⠀⡀⠸⣿⣀⠀⠀⠀⠀⠀⠈⣇⠀
+⡇⠀⡇⠀⠀⡀⠀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠸⡌⣵⡀⢳⡇⠀⠀⠀⠀⠀⠀⢹⡀
+⡇⠀⠇⠀⠀⡇⡸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠮⢧⣀⣻⢂⠀⠀⠀⠀⠀⠀⢧
+⣇⠀⢠⠀⠀⢳⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡎⣆⠀⠀⠀⠀⠀⠘
+⢻⠀⠈⠰⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⠘⢮⣧⡀⠀⠀⠀⠀
+⠸⡆⠀⠀⠇⣾⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠆⠀⠀⠀⠀⠀⠀⠀⠙⠳⣄⡀⢢⡀
+ oregairu */ 
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long 
+#define co cout<< 
+#define rep(i,x,n) for(long long i = x; i < n; i++) 
+#define rev(i,n,x) for(long long i = n-1; i >= x; --i)
+#define ff            first
+#define ss            second 
+#define pii pair<long long ,long long > 
+#define pb                push_back 
+#define ppb               pop_back 
+#define all(x)          (x).begin(),(x).end()
+#define rall(x)          (x).rbegin(),(x).rend()
+#define sz(x)           (x).size() 
+#define DEBUG(x) cout << (x) << '\n';
+#define LSB(i) ((i) & -(i))
+#define int long long 
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL)
+const ll oo = 1e18;
+inline void yn(int x) { if(x)co "YES\n"; else co "NO\n"; return; }
+
+  const int N = 1e5 + 2 ,M=1e6 ,mod=1e9+7,mod1=998244353;
+  const int MAXA = 0; 
+ // int mind[MAXA+5]={0};
+ /*  
+  
+int binpow (int a, int n) {
+    int res = 1;
+    while (n) {
+        if (n & 1)
+            res = 1ll * res * a % base;
+        a = 1ll * a * a % base;
+        n >>= 1;
+    }
+    return res;
+}
+ 
+*/
+/*  
+ll gcd(ll u, ll v)
+{
+    int shift;
+    if (u == 0) return v;
+    if (v == 0) return u;
+    shift = __builtin_ctz(u | v);
+    u >>= __builtin_ctz(u);
+    do {
+        v >>= __builtin_ctz(v);
+        if (u > v) {
+            ll t = v;
+            v = u;
+            u = t;
+        }  
+        v = v - u;
+    } while (v != 0);
+    return u << shift;
+}
+*/ 
+ll n,ans=0 ,tmp,m,q ;string s;  
+vector<vector<int>> dp;
+int calc(int l, int r){ 
+    int &res = dp[l][r];
+    if(res != -1) return res;
+    
+    if(l > r) return res = 0;
+    if(l == r) return res = 1;
+    
+    res = 1 + calc(l + 1, r);
+    for(int i = l + 1; i <= r; ++ i)
+        if(s[l] == s[i])
+            res = min(res, calc(l + 1, i - 1) + calc(i, r));
+    return res;
+}
+void solve(){
+cin>> n ; 
+cin>>s;
+ s=' '+s;
+ dp=vector<vector<int>>(n+1,vector<int>(n+1,-1));
+ 
+DEBUG(calc(1, n))
+}
+
+signed main(){   
+ /*
+for (int i = 2; i * i < MAXA; i++) {
+    if (mind[i] == 0) {
+      mind[i] = i;
+      for (int j = i * i; j < MAXA; j += i) {
+        if (mind[j] == 0) {
+          mind[j] = i;
+        }
+      }
+    }
+  }
+ */
+  
+int tc=1;    // cin>> tc; 
+while(tc--) { 
+
+solve();
+
+ 
+}return 0;} 
