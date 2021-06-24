041f3f58395f1d1a2a52e36218ccf19865bba4a4
jayendra <65066359+jdragonjds@users.noreply.github.com>
2021-06-24 22:06:05 +0530

    Create 613 (Div. 2), problem: (D) Dr. Evil Underscores.cpp

---
+++ binary,constructive ,1700-1900/613 (Div. 2), problem: (D) Dr. Evil Underscores.cpp
@@ -0,0 +1,102 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long 
+#define co cout<< 
+#define ci cin>> 
+#define rep(i,x,n) for(long long i = x; i < n; i++) 
+#define rev(i,x,n) for(long long i = n-1; i >= x; --i)
+#define ff            first
+#define ss            second 
+#define pii pair<long long ,long long > 
+#define pb                push_back  
+#define all(x)          (x).begin(),(x).end()
+#define rall(x)          (x).rbegin(),(x).rend()
+#define sz(x)           (x).size() 
+#define DEBUG(x) cout << (x) << '\n';
+#define LSB(i) ((i) & -(i))
+#define int  long long
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef vector <int> vec;
+const ll oo = 1e18;
+inline void yn(int x) { if(x)co "YES\n"; else co "NO\n"; return; }
+
+  const int N = 1e5 + 2 ,M=1e6 ,mod=1e9+7,mod1=998244353;
+  const int MAXA = 0;const int MAXN = 1e6 + 239; 
+ // int mind[MAXA+5]={0};
+ /*  
+  
+int binpow (int a, int n) {
+    int res = 1;
+    while (n) {
+        if (n & 1)
+            res = 1ll * res * a % base;
+        a = 1ll * a * a % base;
+        n >>= 1;
+    }
+    return res;
+}
+ 
+*/
+/*  
+ll gcd(ll u, ll v)
+{
+    int shift;
+    if (u == 0) return v;
+    if (v == 0) return u;
+    shift = __builtin_ctz(u | v);
+    u >>= __builtin_ctz(u);
+    do {
+        v >>= __builtin_ctz(v);
+        if (u > v) {
+            ll t = v;
+            v = u;
+            u = t;
+        }  
+        v = v - u;
+    } while (v != 0);
+    return u << shift;
+}
+*/
+int n ,m ; 
+vector <int> a;
+
+
+int solve(vector <int> &c, int bit){
+    if(bit < 0) return 0;
+    vector <int> l, r;
+    for(auto &i : c){
+        if(((i >> bit) & 1) == 0) l.push_back(i);
+        else r.push_back(i);
+    }
+    if(l.size() == 0) return solve(r, bit - 1);
+    if(r.size() == 0) return solve(l, bit - 1);
+    return min(solve(l, bit - 1), solve(r, bit - 1)) + (1 << bit);
+}//solve
+
+signed main(){ fast;  
+ /*
+for (int i = 2; i * i < MAXA; i++) {
+    if (mind[i] == 0) {
+      mind[i] = i;
+      for (int j = i * i; j < MAXA; j += i) {
+        if (mind[j] == 0) {
+          mind[j] = i;
+        }
+      }
+    }
+  }
+ */
+
+int tc=1;        // cin>> tc; 
+while(tc--) { 
+
+ cin >> n;
+    a.resize(n);
+    for(auto &i : a) cin >> i;
+    cout << solve(a, 30) << endl;
+
+ 
+}return 0;} 
