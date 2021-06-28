61ea5c5d7b425e2f2d63a4d33f588999b9902aa1
jayendra <65066359+jdragonjds@users.noreply.github.com>
2021-06-28 21:52:19 +0530

    best tree dp hard

---
+++ epic 2000+/263 (Div. 1), problem: (B) Appleman and Tree.cpp
@@ -0,0 +1,114 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include "bits/stdc++.h"
+using namespace std;
+#define ll long long 
+#define co cout<< 
+#define ci cin>> 
+#define rep(i,x,n) for(long long i = x; i < n; i++) 
+#define rev(i,x,n) for(long long i = n-1; i >= x; --i)
+#define ff            first
+#define ss            second 
+#define pii pair<long long ,long long > 
+#define pb                push_back  
+#define all(x)          (x).begin(),(x).end()
+#define rall(x)          (x).rbegin(),(x).rend()
+#define sz(x)           (x).size() 
+#define DEBUG(x) cout << (x) << '\n';
+#define LSB(i) ((i) & -(i))
+#define int  long long
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef vector <int> vec;
+const ll oo = 1e18;
+inline void yn(int x) { if(x)co "YES\n"; else co "NO\n"; return; }
+
+  const int N = 1e5 + 2 ,M=1e6 ,mod=1e9+7,mod1=998244353;
+  const int MAXA = 0;const int MAXN = 1e6 + 239; 
+ // int mind[MAXA+5]={0};
+ /*  
+  
+int binpow (int a, int n) {
+    int res = 1;
+    while (n) {
+        if (n & 1)
+            res = 1ll * res * a % base;
+        a = 1ll * a * a % base;
+        n >>= 1;
+    }
+    return res;
+}
+ 
+*/
+/*  
+ll gcd(ll u, ll v)//binary gcd
+{
+    int shift;
+    if (u == 0) return v;
+    if (v == 0) return u;
+    shift = __builtin_ctz(u | v);
+    u >>= __builtin_ctz(u);
+    do {
+        v >>= __builtin_ctz(v);
+        if (u > v) {
+            ll t = v;
+            v = u;
+            u = t;
+        }  
+        v = v - u;
+    } while (v != 0);
+    return u << shift;
+}
+*/
+ll n,m;  
+int c[N],dp[N][2]; 
+vector<vector<int>> g;
+void dfs(int z,int p=-1){
+    dp[z][0] = 1;
+    dp[z][1] = 0;
+for(auto x:g[z]){if(x==p)continue;
+  dfs(x,z);
+  dp[z][1] *= dp[x][0];
+  dp[z][1] += dp[z][0]*dp[x][1];
+  dp[z][0] *= dp[x][0];
+}
+ if (c[z] == 1)
+  dp[z][1] = dp[z][0];
+ else 
+  dp[z][0] += dp[z][1];
+}
+ 
+void solve()
+{ cin >> n ; 
+    g=vector<vector<int>>(n+1);
+ rep(i,1,n){
+    int u; ci u;g[u].pb(i);g[i].pb(u);
+ }
+ rep(i,0,n){
+    ci c[i];
+ }dfs(1);
+co dp[1][1];
+
+}//solve
+
+signed main(){ fast;  
+ /*
+for (int i = 2; i * i < MAXA; i++) {
+    if (mind[i] == 0) {
+      mind[i] = i;
+      for (int j = i * i; j < MAXA; j += i) {
+        if (mind[j] == 0) {
+          mind[j] = i;
+        }
+      }
+    }
+  }
+ */
+
+int tc=1;      //  cin>> tc; 
+while(tc--) { 
+
+solve();
+
+ 
+}return 0;} 
